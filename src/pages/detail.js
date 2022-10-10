3befdb4087c9a21d287489df61b55dd3b6defafb
yskim <yt.wati@gmail.com>
2022-10-10 18:15:14 +0900

    상세페이지 find 활용

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,16 +1,23 @@
+import { useParams } from 'react-router-dom';
+
+// 전달할 수 있는 데이터는 props에서 전달 상위에서 한번에 관리하기 좋음
+function DetailPage(props) {
+
+    const { id } = useParams();
+
+    const found = props.shoes.find(item => item.id == id);
 
 
-function DetailPage() {
     return (
         <div className="container">
         <div className="row">
             <div className="col-md-6">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+                <img src={`https://codingapple1.github.io/shop/shoes${1 + parseInt(id)}.jpg`} alt='ShoeImg' width="100%" />
             </div>
-            <div className="col-md-6">
-            <h4 className="pt-5">상품명</h4>
-            <p>상품설명</p>
-            <p>120000원</p>
+            <div className="col-md-6 mt-4">
+            <h4 className="pt-5">{found.title}</h4>
+            <p>{found.content}</p>
+            <p>{found.price}원</p>
             <button className="btn btn-danger">주문하기</button> 
             </div>
         </div>

cff70967e9115fd3c2d3a4ab9c16faf3eb3bf354
yskim <yt.wati@gmail.com>
2022-10-10 11:38:36 +0900

    card component 만들기 완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js'
+
 
 function App() {
+
+  let [shoes] = useState(data)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">TMITEM</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          {shoes.map((shoe,i)=>(<ItemCard title={shoe.title} price={shoe.price} imageNum={i+1} />))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ItemCard(props) {
+  return (
+    <div className="ItemCard">
+      <div className="col-md-4">
+        <img src={`https://codingapple1.github.io/shop/shoes${props.imageNum}.jpg`} alt='shoeImg' width='80%' />
+        <h4>{props.title}</h4>
+        <p>{props.price}원</p>
+      </div>
     </div>
   );
 }

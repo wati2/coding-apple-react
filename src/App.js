f06bf4e6f5119ebaa07cde83198744534e3186b0
yskim <yt.wati@gmail.com>
2022-10-10 17:39:54 +0900

    Route 과제

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useState } from 'react';
 import data from './data.js'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 import DetailPage from './pages/detail'
 
 function App() {
 
   let [shoes] = useState(data)
+  // hook
+  let navigate = useNavigate();
+  
 
   return (
     <div className="App">
@@ -16,9 +19,9 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">TMITEM</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            {/* navigate(1) 앞으로한페이지, navigate(-1) 뒤로한페이지 */}
+            <Nav.Link onClick={ ()=>{navigate('/')} }>Home</Nav.Link>
+            <Nav.Link onClick={ ()=>{navigate('/detail')} }>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -43,6 +46,15 @@ function App() {
 
         {/* Detail 페이지 */}
         <Route path="/detail" element={<DetailPage/>} />
+        <Route path='/about' element={<About/>}>
+          <Route path='member' element={<div>멤버임</div>} />
+          <Route path='location' element={<div>위치정보임</div>} />
+        </Route>
+        <Route path='/event' element={<DailyEvent/>}>
+          <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path='two' element={<div>생일기념 쿠폰받기</div>} />
+        </Route>
+        <Route path='*' element={<div>** Nothing **</div>} />
       </Routes>
       
     </div>
@@ -59,6 +71,25 @@ function ItemCard(props) {
       </div>
     </div>
   );
+}
+
+function About() {
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      {/* 어디보여줄지 정하는 곳 */}
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function DailyEvent() {
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 export default App;

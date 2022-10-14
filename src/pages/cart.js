1ffea5d528ad0449d32ded556c5a09dd9734636c
yskim <yt.wati@gmail.com>
2022-10-14 18:04:07 +0900

    useMemo

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -3,6 +3,16 @@ import { Table } from 'react-bootstrap'
 import { useSelector, useDispatch } from 'react-redux'
 import { addCount } from './../store.js'
 import { changeName, chageAge } from './../store/userSlice.js'
+import { memo, useState } from 'react'
+
+// 꼭 필요할때만 재렌더링 하기위한 memo, 무거운 컴포넌트 재렌더링 방지
+// memo의 원리 props가 변할때만 재렌더링해줌
+// 무조건 쓰는건 문제가 있음, 기존 props, 신규props 계속 비교할듯 성능에 영향을 줌
+let Child = memo(function(){
+  console.log('재렌더링됨')
+  return <div>자식임</div>
+})
+
 
 function Cart(){
 
@@ -11,10 +21,12 @@ function Cart(){
   let cart = useSelector((state)=> state.cart)
   let user = useSelector((state)=> state.user)
   let dispatch = useDispatch() // store.js로 요청보내주는 함수임
+  let [count, setCount] = useState(0)
 
   return (
     <div>
-
+      <Child></Child>
+      <button onClick={()=>{ setCount(count+1) }}>+</button>
       <span><strong>{user.name}</strong>의 장바구니, {user.age}
       </span>
       <div>
